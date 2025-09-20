cec12090193ad0331dc62fc2217ec1e04a2e14dc
Uday Kumar Maurya <motuaurpatlukijodi25@gmail.com>
2025-09-20 20:37:32 +0530

    changes index.html

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,12 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173", 
-  "https://the-watchlist-graveyard.onrender.com"
-];
-
 app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like mobile apps or curl)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true, // if you use cookies or auth headers
+  origin: [
+    "http://localhost:5173", // for local dev
+    "https://the-watchlist-graveyard.onrender.com" // your deployed frontend
+  ],
+  credentials: true, // only if you use cookies/auth headers
 }));
 
 app.use(express.json());
