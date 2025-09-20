41fabc48b070e6b5daf5a515a73fe55d21fbaead
Uday Kumar Maurya <motuaurpatlukijodi25@gmail.com>
2025-09-20 20:13:51 +0530

    Added now

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://bingewatchers-1.onrender.com", 
   methods: ["GET","POST","DELETE","PUT","PATCH"]
 }));
 
